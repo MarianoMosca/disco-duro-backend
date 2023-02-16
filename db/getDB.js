1ecf8267e1f23164d7cbeaed9d277c402b89323a
mariano <marianomosca96@gmail.com>
2023-02-16 18:26:33 +0100

    discoDuro

---
+++ db/getDB.js
@@ -0,0 +1,27 @@
+const mysql = require("mysql2/promise");
+
+const { MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE } = process.env;
+
+let pool;
+
+const getDB = async () => {
+  try {
+    if (!pool) {
+      pool = mysql.createPool({
+        connectionLimit: 10,
+        host: MYSQL_HOST,
+        user: MYSQL_USER,
+        password: MYSQL_PASSWORD,
+        database: MYSQL_DATABASE,
+        timezone: "Z",
+      });
+    }
+
+    return await pool.getConnection();
+  } catch (err) {
+    console.error(err);
+    throw new Error("Error al conectar con la base de datos");
+  }
+};
+
+module.exports = getDB;
---
+++ server.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(morgan("dev"));
+
+// Middleware gestion de errores.
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.httpStatus || 500).send({
+    status: "error",
+    message: err.message,
+  });
+});
+
+// Middleware 404.
+app.use((req, res) => {
+  res.status(404).send({
+    status: "error",
+    message: "Ruta no encontrada ",
+  });
+});
+
+// Lanzamos el server.
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening http://localhost:${process.env.PORT}`);
+});
