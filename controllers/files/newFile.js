7706dedcb4039f5ad7347a6b8105faf2e2eebc7c
mariano <marianomosca96@gmail.com>
2023-02-21 20:22:19 +0100

    archivo subido

--- controllers/files/newFile.js
+++ controllers/files/newFile.js
@@ -4,12 +4,12 @@ const { generateError, saveFile } = require("../../helpers");
 
 const newFile = async (req, res, next) => {
   try {
-    const { file } = req.params;
+    const { fileName } = req.params;
 
-    if (!req.files.file) {
+    if (!req.files?.file) {
       generateError("Falta el archivo", 400);
     }
-
+    await saveFile(fileName);
     res.send({
       status: "ok",
       message: "Archivo subido exitosamente",
