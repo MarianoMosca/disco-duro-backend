6f63d691b15754dd9fef53910479ee4c6bc95865
Juanmargui <117675187+Juanmargui@users.noreply.github.com>
2023-02-24 21:02:12 +0100

    queries/folders

--- foldersQ/insertFileQuery.js
+++ foldersQ/insertFileQuery.js
@@ -1,4 +1,5 @@
 "use strict";
+const { generateError } = require("../../../helpers");
 const getDB = require("../../getDB");
 
 const insertFileQuery = async (name, idFolder) => {
@@ -7,11 +8,18 @@ const insertFileQuery = async (name, idFolder) => {
   try {
     connection = await getDB();
 
+    const [files] = await connection.query(
+      `SELECT id FROM files where idFolder = ?`,
+      [idFolder]
+    );
+    if (files.length > 2) {
+      generateError("No es posible agregar más de tres ficheros", 403);
+    }
+
     const [newFile] = await connection.query(
-      `INSERT INTO files (name, idFolder) VALUES (?, ?)`,
+      `INSERT INTO files (name, idFolder) VALUES (?,?)`,
       [name, idFolder]
     );
-
     return newFile.insertId; //para saber el id que el fichero tiene en la base de datos
   } finally {
     if (connection) connection.release();
